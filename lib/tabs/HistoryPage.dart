67bfda87753c6df2cb600d1d740eacde6bc959da
DinukaUdawela <126551484+DinukaUdawela@users.noreply.github.com>
2023-12-02 00:05:50 +0530

    4th commit

--- lib/tabs/HistoryPage.dart
+++ lib/tabs/HistoryPage.dart
@@ -1,15 +1,68 @@
 import 'package:flutter/material.dart';
 
 class HistoryPage extends StatefulWidget {
-  const HistoryPage({super.key});
+  const HistoryPage({Key? key}) : super(key: key);
 
   @override
   State<HistoryPage> createState() => _HistoryPageState();
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  List<String> tasks = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(child: Text("History Page"));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('History Page with Tasks'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(tasks[index]),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      // Remove the task when the delete icon is pressed
+                      setState(() {
+                        tasks.removeAt(index);
+                      });
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              onFieldSubmitted: (value) {
+                // Add the task when the user submits the text field
+                setState(() {
+                  tasks.add(value);
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Add a new task',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    // Add the task when the add icon is pressed
+                    setState(() {
+                      tasks.add(tasks.length
+                          .toString()); // You can replace this with the actual task content
+                    });
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
